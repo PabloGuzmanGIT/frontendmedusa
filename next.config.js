76a8442751da6eec1e728ace32a54ab9f93f18c6
PabloGuzmanGIT <guzmagp23@gmail.com>
2025-09-26 00:45:59 -0500

    fixing front de0ployed

--- next.config.js
+++ next.config.js
@@ -44,7 +44,9 @@ const nextConfig = {
     ],
   },
   experimental:{
-    
+    optimizeCss: true, // Add this to reduce CSS preload warnings
+    serverComponentsExternalPackages: ['@medusajs/framework', 'medusa-core-utils'], // If using Medusa
+    esmExternals: 'loose',
   }
 }
 
